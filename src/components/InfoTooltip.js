b6200954e5f73611f66c8e828cc1f7faefbbf410
Vladimir Blinov <zemetrans21@yandex.ru>
2021-06-27 20:33:14 +0300

    feat: сделать основную вёрстку. Необходимо вынести ещё в БЭМ структуру

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,8 +1,11 @@
+import succesPic from '../images/auth-success.svg';
+import failPic from '../images/auth-failed.svg';
+
 function InfoToolTip() {
   return (
-    <div className={`popup info-tool-tip-popup popup_status_active`}>
+    <div className={`popup info-tool-tip-popup`}>
       <div className="info-tool-tip-popup__container">
-        <img src="../images/auth-success.svg" alt="Картинка успешной авторизации" className="info-tool-tip-popup__picture"/>
+        <img src={succesPic} alt="Картинка успешной авторизации" className="info-tool-tip-popup__picture"/>
         <p className="info-tool-tip-popup__description">Вы успешно зарегистрировались!</p>
         <button aria-label="Закрыть" type="button" className="popup__close-btn info-tool-tip-popup__close-button" ></button>
       </div>
